74cd86c6b133d5a6fe376a36b07afc3ae27927fc
Sushil pawar <sushilpawar2321@gmail.com>
2026-06-14 11:06:20 +0530

    Time: 1 ms (54.28%), Space: 9.7 MB (49.04%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<int, int> mp;
+        for (auto i : s) {
+            mp[i]++;
+        }
+        for (auto i : t) {
+            mp[i]--;
+        }
+        for (auto it : mp) {
+            if (it.second != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
